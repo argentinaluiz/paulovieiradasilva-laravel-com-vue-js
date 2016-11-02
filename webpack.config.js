63d44b84ff2b1fa424e8d83e83c0b502dd009f6f
vagrant <vagrant@homestead>
2016-11-02 22:56:06 +0000

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		admin: './resources/assets/admin/js/admin.js'
+	},
+	output: {
+		path: __dirname + '/public/build',
+		filename: '[name].bundle.js',
+		publicPath: '/build/'
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			'window.$': 'jquery',
+			'window.jQuery': 'jquery'
+		})
+	],
+	module: {
+		loaders: [
+			{
+				text: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+            loader: 'babel'
+			},
+			{
+				text: /\.vue$/,
+				loader: 'vue',
+			},
+		]
+	}
+};
