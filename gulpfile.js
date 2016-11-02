95e8d49bb857eca7f1dcd0db548f3a717b99175b
vagrant <vagrant@homestead>
2016-11-02 22:37:36 +0000

    Configurando Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -16,4 +16,9 @@ require('laravel-elixir-vue');
 elixir(mix => {
     mix.sass('./resources/assets/admin/sass/admin.scss')
        .copy('./node_modules/materialize-css/fonts/roboto', './public/fonts/roboto');
+
+    mix.browserSync({
+        host: '0.0.0.0',
+        proxy: 'http://192.168.10.10:8000'
+    });
 });
